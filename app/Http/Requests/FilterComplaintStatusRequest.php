b5c8a7706fb427771bef0e4f42f3b2ec19292d8a
stringAsmaa <152689060+stringAsmaa@users.noreply.github.com>
2025-12-08 23:15:16 +0300

    edit filter complaints

--- app/Http/Requests/FilterComplaintStatusRequest.php
+++ app/Http/Requests/FilterComplaintStatusRequest.php
@@ -24,7 +24,7 @@ class FilterComplaintStatusRequest extends FormRequest
     {
        return [
             'status' => [
-                'required',
+                'sometimes',
                 'in:' . implode(',', array_column(ComplaintStatusEnum::cases(), 'value')),
             ],
         ];
