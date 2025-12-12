8adcad61649436199514470b545eef1a84030328
stringAsmaa <asmaamh151@gmail.com>
2025-12-12 16:16:19 +0300

    DockerFile

---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+echo "Waiting for MySQL to be ready..."
+while ! mysqladmin ping -h "$DB_HOST" -P "$DB_PORT" --silent; do
+    echo "MySQL is unavailable - sleeping"
+    sleep 3
+done
+
+echo "MySQL is ready! Running migrations..."
+mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME" -p"$DB_PASSWORD" -e "SET FOREIGN_KEY_CHECKS=0;"
+
+php artisan db:wipe --force
+php artisan migrate:fresh --force
+mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME" -p"$DB_PASSWORD" -e "SET FOREIGN_KEY_CHECKS=1;"
+
+echo "Seeding database..."
+php artisan db:seed --force
+
+
+
+# تشغيل السيرفر
+echo "Starting Laravel server..."
+exec php -S 0.0.0.0:$PORT -t public
